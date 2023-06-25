83eee05e01af8c4eee9462f250139b9f72174b98
official-okello <officialokello@gmail.com>
2023-06-25 12:23:34 +0300

    It all works

---
+++ nop.c
@@ -0,0 +1,13 @@
+#include "monty.h"
+
+/**
+  *f_nop- nada
+  *@head: stack head
+  *@counter: line number
+  *Return: None
+ */
+void f_nop(stack_t **head, unsigned int counter)
+{
+	(void) counter;
+	(void) head;
+}
---
+++ stack.c
@@ -0,0 +1,14 @@
+#include "monty.h"
+
+/**
+ * f_stack - prints the top
+ * @head: stack head
+ * @counter: line number
+ * Return: None
+*/
+void f_stack(stack_t **head, unsigned int counter)
+{
+	(void)head;
+	(void)counter;
+	bus.lifi = 0;
+}
